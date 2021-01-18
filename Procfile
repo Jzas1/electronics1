9df61f8faf35c243d2f22a75f20a4ae71ab85c59
JoeZ <josephzasiebida@gmail.com>
2021-01-18 15:27:40 -0600

    'new'

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: node backend/server.js
+heroku config:set NPM_CONFIG_PRODUCTION=false
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ import productRoutes from './routes/ProductRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/orderRoutes.js'
-
 dotenv.config()
 
 connectDB()
@@ -22,34 +21,38 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
-
+app.get('/api/config/paypal', (req, res) =>
+    res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
 } else {
     app.get('/', (req, res) => {
-        res.send('API is running')
+        res.send('API is running....')
     })
 }
 
 app.use(notFound)
 app.use(errorHandler)
 
-
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-    .yellow.bold
-))
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+)
